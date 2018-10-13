c5797f315d3bb64af4b69e75515e16ff0862fe61
David Souza <souzanaranjo@gmail.com>
2018-10-13 18:45:06 +0800

    Enhanged profile page and added link to edit profile page. Also rearranged some functions

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-window.fn = {};
-
-window.fn.toggleMenu = function () {
-  document.getElementById('appSplitter').right.toggle();
-};
-
-window.fn.loadView = function (index) {
-  document.getElementById('appTabbar').setActiveTab(index);
-  document.getElementById('sidemenu').close();
-};
-
-window.fn.loadLink = function (url) {
-  window.open(url, '_blank');
-};
-
-window.fn.pushPage = function (page, anim) {
-  if (anim) {
-    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
-  } else {
-    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
-  }
-};
-
-// Code related to map.
-
-
-miArreglo = [
-  	  ['Country', 'Popularity'],
-      ['Germany', 700],
-      ['Brazil', 700],
-      ['Canada', 700],
-      ['France', 700],
-      ['RU', 700]
+// Profile information.
+// TODO Fetch info from the user
+var user_name = "David Souza";
+var user_description = "CS Student";
+var user_phonenumber = "+65 12348765"
+var user_nationality = "Mexican";
+var user_languages = ["English", "Spanish", "Italian", "French"];
+var user_interests = ["Sports", "Play the piano", "Science", "Raggaeton Music"];
+var countries = [
+	  ['Country', 'Popularity'],
+  	  ['Default', 0],
+      ['Germany', 1],
+      ['Brazil', 1],
+      ['Canada', 1],
+      ['France', 1],
+      ['RU', 2]
     ];
-    // miArreglo.concat([['Mexico', 700]]);
 
-google.charts.load('current', {
+function loadUserInfo() {
+	document.getElementById("user-name").innerHTML = user_name;
+	document.getElementById("user-description").innerHTML = user_description;
+	document.getElementById("user-phonenumber").innerHTML = user_phonenumber;
+	document.getElementById("user-nationality").innerHTML = user_nationality;
+	document.getElementById("user-languages").innerHTML = "";
+	for (language of user_languages) {
+		document.getElementById("user-languages").innerHTML += "- " + language;
+	}
+	document.getElementById("user-interests").innerHTML = "";
+	for(interest of user_interests) {
+		document.getElementById("user-interests").innerHTML += "- " + interest;
+	}
+	loadMap();
+};
+
+function loadMap() {
+	google.charts.load('current', {
         'packages':['geochart'],
         // Note: you will need to get a mapsApiKey for your project.
         // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
@@ -44,10 +43,12 @@ google.charts.load('current', {
       google.charts.setOnLoadCallback(drawRegionsMap);
 
       function drawRegionsMap() {
-      	miArreglo.concat([["United States", 700]]);
-        var data = google.visualization.arrayToDataTable(miArreglo);
+        var data = google.visualization.arrayToDataTable(countries);
         var options = {};
         var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
         chart.draw(data, options);
       }
+}
+
+// End of code related to map
